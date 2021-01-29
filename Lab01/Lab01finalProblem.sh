5ea0447eebde40d543be73c9506abec219b5d3cf
aukr6487 <aukr6487@colorado.edu>
2021-01-29 11:17:50 -0700

    Adding clarifying note to shell script

--- Lab01/Lab01finalProblem.sh
+++ Lab01/Lab01finalProblem.sh
@@ -5,3 +5,6 @@
 grep time PredPreyData.csv > PredPreyDataCUT.csv
 tail PredPreyData.csv >> PredPreyDataCUT.csv
 cut -f 2-4 -d , PredPreyDataCUT.csv > PredPreyDataSmall.csv
+
+# PredPreyDataSmall.csv is the  new .csv file with 11 lines of data total and three columns total, in which the top line should be the original headers, and the other 10 lines should be the last 10 lines of the original data
+
